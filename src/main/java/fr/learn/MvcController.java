76396d2227695dda975c570386bff8131972c9fb
ider <id.toufik@hotmail.com>
2017-02-25 14:40:15 +0100

    memberService presque finie

--- src/main/java/fr/learn/MvcController.java
+++ src/main/java/fr/learn/MvcController.java
@@ -1,7 +1,10 @@
 package fr.learn;
 
+import java.security.Principal;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/fr/learn/member/MemberController.java
+++ src/main/java/fr/learn/member/MemberController.java
@@ -3,6 +3,9 @@ package fr.learn.member;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,27 +29,72 @@ public class MemberController {
 	@RequestMapping(value = "/resources/members", method = RequestMethod.GET)
 	public List<Member> getAllMembers()
 	{
-		return memberService.findAll();
+		List<Member> members = memberService.findAll();
+		members.forEach(t -> t.setPassword(null));
+		return members;
 	}
 	
+	
+	//TODO only the superUser can drop users
 	@RequestMapping(value = "/resources/members/{idMember}", method = RequestMethod.DELETE)
 	public void deleteMember(@PathVariable("idMember") long idMember)
 	{
 		memberService.delete(idMember);
 	}
 	
-	
 	@RequestMapping(value = "/resources/members/{idMember}", method = RequestMethod.GET)
 	public Member getMember(@PathVariable("idMember") long idMember)
 	{
-		return memberService.getMember(idMember);
+		Member member = memberService.getMember(idMember);
+		member.setPassword(null);
+		return member;
 	}
 	
+	
+	//TODO mettre a jour user detailsService apres la mise a jour du model 
+	/*
+	 * 
+	 * Authentication auth = SecurityContextHolder.getContext().setAuthentication();
+	        if(auth.getPrincipal() != null)
+	        {
+	        	
+	        	UserDetails details = (UserDetails)auth.getPrincipal();
+	        	System.out.println(details.getUsername());
+	        	details.setUsername(details.getUsername() +"1");
+	        	
+	        }
+	 */
 	@RequestMapping(value = "/resources/members/{idMember}", method = RequestMethod.PUT)
 	public void updateMember(@PathVariable("idMember") long idMember, @RequestBody Member member)
 	{
-		member.setId(idMember);
-		member.setPassword(memberService.findOne(idMember).getPassword());
-		memberService.updateMember(member);
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Member loggedIn = memberService.getMemberFromAuthentification(auth);
+		if(loggedIn != null && loggedIn.getId() == idMember)
+		{
+			member.setId(idMember);
+			member.setPassword(memberService.findOne(idMember).getPassword());
+			memberService.updateMember(member);
+		}
 	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
