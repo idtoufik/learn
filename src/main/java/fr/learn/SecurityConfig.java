51a1981616a54c753b0d9cbb52fd3f5ca7164a09
ider <id.toufik@hotmail.com>
2017-02-27 13:40:14 +0100

    suppression thymleaf

--- src/main/java/fr/learn/SecurityConfig.java
+++ src/main/java/fr/learn/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
              .anyRequest().authenticated()
              .and()
          .formLogin()
-             .loginPage("/login")
+             .loginPage("/login.html")
              .permitAll()
              .and()
          .logout()
